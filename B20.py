cfa38cad61a886c9789b604e58c4865f225f7053
eddie1711 <dangtrieudung2007@gmail.com>
2023-02-03 22:12:07 +0700

    added B20.py

---
+++ B20.py
@@ -0,0 +1,5 @@
+for i in range(1, 21) :
+     for j in range(1, 34) :
+          k = 100 - i - j
+          if 5*i + 3j + k/3 == 100 :
+          print( "Trâu đứng ",i," trâu nằm ",j," trâu già ",k)

33a41be9aaa1c3c8bb7667ac4b55746f7a157fdf
eddie1711 <dangtrieudung2007@gmail.com>
2023-03-12 23:25:17 +0700

    B27 change

--- DeCuong/B27WHILE.py
+++ DeCuong/B27WHILE.py
@@ -1,7 +1,7 @@
 def int_to_rome(x):
     numbers = [1,4,5,9,10,40,50,90,100,400,500,900,1000]
     roman = ['I','IV','V','IX', 'X','XL', 'L','XC', 'C','CD', 'D','CM','M']
-    i = 12  
+    i = 12 
     roman_numeral = ''
     while x != 0:
         if numbers[i] <= x:    
